1721d6edeaf847f6fc8e5a1720aec1a18fe28fff
Dristi <usrdristi@gmail.com>
2025-04-12 22:56:50 +0600

    add scholarship and probation

--- Lab1-Studenet_Dept_Management/Lab1-Studenet_Dept_Management/Controllers/StudentController.cs
+++ Lab1-Studenet_Dept_Management/Lab1-Studenet_Dept_Management/Controllers/StudentController.cs
@@ -14,7 +14,25 @@ namespace Lab1_Studenet_Dept_Management.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            var students = db.Students.Include("Department").ToList();  
+            var students = db.Students.ToList();  
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult Scholarships()
+        {
+            var students = (from s in db.Students
+                            where s.Cgpa >= 3.75
+                            select s).ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult Probation()
+        {
+            var students = (from s in db.Students
+                            where s.Cgpa < 2.00
+                            select s).ToList();
             return View(students);
         }
 
@@ -27,13 +45,8 @@ namespace Lab1_Studenet_Dept_Management.Controllers
 
         [HttpPost]
         public ActionResult CreateStudent(Student s)
-        {
-            Student std = new Student();
-            std.Name = s.Name;
-            std.Cgpa = s.Cgpa;
-            std.D_Id = s.Id;
-
-            db.Students.Add(std);
+        {        
+            db.Students.Add(s);
             if (db.SaveChanges() > 0)
             {
                 TempData["Msg"] = "Student updated Successfully.";
@@ -43,6 +56,70 @@ namespace Lab1_Studenet_Dept_Management.Controllers
             {
                 TempData["Msg"] = "Failed to update student.";
                 return View(s);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EditStudent(int id)
+        {
+            var student = db.Students.Find(id);
+            var depts = db.Departments.ToList();
+
+            ViewBag.Dept = depts;
+            return View(student);
+
+        }
+
+        [HttpPost]
+        public ActionResult EditStudent(Student s)
+        {
+            var prevS = db.Students.Find(s.Id);
+
+            if (prevS != null)
+            {
+                prevS.Name = s.Name;
+                prevS.Cgpa = s.Cgpa;
+                prevS.D_Id = s.D_Id;
+                if (db.SaveChanges() > 0)
+                {
+                    TempData["Msg"] = "Student updated Successfully.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Msg"] = "Failed to update student.";
+                    return View(s);
+                }
+            }
+            else
+            {
+                TempData["Msg"] = "Student not found.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteStudent(int id)
+        {
+            var student = db.Students.Find(id);
+            if (student != null)
+            {
+                db.Students.Remove(student);
+                if (db.SaveChanges() > 0)
+                {
+                    TempData["Msg"] = "Student deleted Successfully.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Msg"] = "Failed to delete student.";
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                TempData["Msg"] = "Student not found.";
+                return RedirectToAction("Index");
             }
         }
     }
